aca0f4d7b785453bcad0bd73b8e9e34a8600685f
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-06 11:28:59 +0100

    types

--- app/app.ts
+++ app/app.ts
@@ -3,3 +3,16 @@ function startG () {
   messagesElement!.innerText = "start Game";
 }
 document.getElementById('startGame')!.addEventListener('click',startG);
+var testElement = document.getElementById("test");
+let firstName : String = "Hello Dylan ";
+// Implicit any as JSON.parse doesn't know what type of data it returns so it can be "any" thing...
+const json = JSON.parse('{"age" : "55"}');
+// Most expect json to be an object, but it can be a string or a number like this example
+const number = JSON.parse('55');
+// console.log(typeof number);
+
+testElement!.innerText = typeof json + " => age : " +json.age ;
+
+let v: any = true;
+v = "string"; // no error as it can be "any" type
+console.log(typeof v, Math.round(v)); // no error as it can be "any" type
--- js/app.js
+++ js/app.js
@@ -4,4 +4,15 @@ function startG() {
     messagesElement.innerText = "start Game";
 }
 document.getElementById('startGame').addEventListener('click', startG);
+var testElement = document.getElementById("test");
+let firstName = "Hello Dylan ";
+// Implicit any as JSON.parse doesn't know what type of data it returns so it can be "any" thing...
+const json = JSON.parse('{"age" : "55"}');
+// Most expect json to be an object, but it can be a string or a number like this example
+const number = JSON.parse('55');
+// console.log(typeof number);
+testElement.innerText = typeof json + " => age : " + json.age;
+let v = true;
+v = "string"; // no error as it can be "any" type
+console.log(typeof v, Math.round(v)); // no error as it can be "any" type
 //# sourceMappingURL=app.js.map
