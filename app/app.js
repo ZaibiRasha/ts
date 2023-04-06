28a499cb73e853862215e793bac0c8d3105ce0a7
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-06 09:29:35 +0100

    convert ts to js file and run

---
+++ app/app.js
@@ -0,0 +1,5 @@
+function startG() {
+    var messagesElement = document.getElementById("messages");
+    messagesElement.innerText = "start Game";
+}
+document.getElementById('startGame').addEventListener('click', startG);
