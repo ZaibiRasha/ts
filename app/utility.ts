7fa1e3a67e5e1a172ba822e6e6a3d6bfb156fcad
ZaibiRasha <racha.zaibi@wind-consulting-tunisia.com>
2023-04-07 14:26:06 +0100

    modules

--- app/utility.ts
+++ app/utility.ts
@@ -1,7 +1,11 @@
-class Utility {
-  static getInputValue ( elemntID : string ) : string {
-    const postedScores:HTMLInputElement = <HTMLInputElement>document.getElementById(elemntID);
-      return postedScores.value;
-  }
-  
+function getInputValue(elementID: string): string {
+
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+  return inputElement.value;
 }
+
+function logger(message: string): void {
+  console.log(message);
+}
+
+export { getInputValue as getValue, logger }
